1df8b8ff1daf53bd50357d62b211696d66a0cd59
nbhaskareddy <bhaskar.nic.in@gmail.com>
2025-09-18 08:18:06 +0530

    Initial commit

---
+++ Sources/CBProducts/CBProducts.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+@available(macOS 10.15, *)
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! Your Products package is running successfully.")
+        //Test
+    }
+}
+@available(iOS 13.0.0, *)
+@available(macOS 10.15, *)
+#Preview {
+    ContentView()
+}
---
+++ Sources/CBProducts/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  CBProducts
+//
+//  Created by N Bhaskar Reddy on 18/09/25.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(iOS 13.0.0, *)
+#Preview {
+    SwiftUIView()
+}
