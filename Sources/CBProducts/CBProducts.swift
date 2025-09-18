6b9ae306c6d46b2b73aee9c7e2792f6c4ace6a48
nbhaskareddy <bhaskar.nic.in@gmail.com>
2025-09-18 14:02:02 +0530

    Initial commit

--- Sources/CBProducts/CBProducts.swift
+++ Sources/CBProducts/CBProducts.swift
@@ -5,13 +5,19 @@ import SwiftUI
 @available(iOS 13.0.0, *)
 @available(macOS 10.15, *)
 public struct CBProductsView: View {
+    public var title: String
+
+            // Explicitly declare a public initializer
+            public init(title: String) {
+                self.title = title
+            }
     public var body: some View {
-        Text("Hello, World! Your Products package is running successfully.")
+        Text("Hello, World! Your Products package is running successfully. \n\n \(self.title)")
         //Test
     }
 }
 @available(iOS 13.0.0, *)
 @available(macOS 10.15, *)
 #Preview {
-    CBProductsView()
+    CBProductsView(title: "Testing")
 }
